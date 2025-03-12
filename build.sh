0b61e944d5d3392617fe5c1f0a8dac7aaff7984b
Declan Mullen <declanmullen3.14@gmail.com>
2025-03-11 20:49:53 -0700

    feat: added build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source poky/oe-init-build-env
+bitbake core-image-minimal
--- start-docker.sh
+++ start-docker.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 docker build -t poky-builder .
 docker run --rm -it -v ${PWD}:${PWD} -w ${PWD} poky-builder
